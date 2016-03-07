bfc07a23e53527a1d28baa843059c0e2571300ca
Arjun Bali <arjun.bali96@gmail.com>
2016-03-07 00:56:44 -0500

    Artist and track model data member populating

--- app/models.py
+++ app/models.py
@@ -3,47 +3,97 @@ Definition of models.
 """
 
 from django.db import models
-from mongoengine import *
+import mongoengine as me
+import mongoengine.fields as fields
 
 
 # Create your models here.
 
 
-class Employee(Document):
-    email = StringField(require=True)
-    first_name = StringField(max_length=50)
-    last_name = StringField(max_length=50)
+class Employee(me.Document):
+    email = fields.StringField(require=True)
+    first_name = fields.StringField(max_length=50)
+    last_name = fields.StringField(max_length=50)
 
 
-# class Artist(models.Model):
-#     soundcloud_id = models.BigIntegerField()
-#     permalink = models.CharField(max_length=200)
-#     username = models.CharField(max_length=200)
-#     uri = models.URLField(max_length=200)
-#     permalink_url = models.URLField(max_length=200)
-#     avatar_url = models.URLField(max_length=200)
-#     country = models.CharField(max_length=200)
-#     full_name = models.CharField(max_length=200)
-#     city = models.CharField(max_length=200)
-#     description = models.CharField(max_length=500)
-#     discogs_name = models.CharField(max_length=200)
-#     myspace_name = models.CharField(max_length=200)
-#     website = models.URLField(max_length=200)
-#     website_title = models.CharField(max_length=200)
-#     track_count = models.IntegerField()
-#     playlist_count = models.IntegerField()
+class Artist(me.Document):
+    # As per the standard soundcloud /users call
+    soundcloud_id = fields.LongField(require=True)
+    permalink = fields.StringField(max_length=200)
+    username = fields.StringField(max_length=200)
+    uri = fields.URLField(max_length=200)
+    permalink_url = fields.URLField(max_length=200)
+    avatar_url = fields.URLField(max_length=200)
+    country = fields.StringField(max_length=200)
+    full_name = fields.StringField(max_length=200)
+    city = fields.StringField(max_length=200)
+    description = fields.StringField(max_length=500)
+    discogs_name = fields.StringField(max_length=200)
+    myspace_name = fields.StringField(max_length=200)
+    website = fields.URLField(max_length=200)
+    website_title = fields.StringField(max_length=200)
+    # not stored: online | online status (boolean)
+    track_count = fields.IntField()
+    playlist_count = fields.IntField()
+    followers_count = fields.LongField()
+    followings_count = fields.LongField()
+    public_favorites_count = fields.LongField()
+    # not stored: avatar_data | binary data of user avatar
 
-#     # many to many with itself
-#     # has many favourites
-#     pass
-#     # put the web profiles here too
+    # Subresources
+    # tracks = fields.ListField(fields.ReferenceField(Track))
 
 
-# class Tracks(models.Model):
-#     # artist has many tracks - artist foreign key
-#     # has many favouriters
-#     pass
+class Track(me.Document):
+    track_id = fields.LongField(require=True)
+    created_at = fields.DateTimeField()
+    user_id = fields.ReferenceField(Artist)
+    title = fields.StringField()
+    permalink = fields.URLField(max_length=200)
+    uri = fields.URLField(max_length=200)
+    sharing = fields.StringField(max_length=200)
+    embeddable_by = fields.StringField(max_length=200)
+    purchase_url = fields.URLField(max_length=200)
+    artwork_url = fields.URLField(max_length=200)
+    description = fields.StringField(max_length=200)
+    label = fields.ReferenceField(Artist)  #?
+    duration = fields.IntField()
+    genre = fields.StringField()
+    tag_list = fields.ListField()
+    label_id = fields.LongField()  # probably redundant
+    label_name = fields.StringField()  # probably redundant
+    release_number = fields.IntField()
+    release_day = fields.IntField()
+    release_month = fields.IntField()
+    release_year = fields.IntField()
+    streamable = fields.BooleanField()
+    downloadable = fields.BooleanField()
+    encoding_state = fields.StringField()  # either processing, failed, or finished
+    license = fields.StringField()
+    track_type = fields.StringField()
+    # not stored: waveform url (url to png)
+    download_url = fields.URLField()
+    stream_url = fields.URLField()
+    video_url = fields.URLField()
+    bpm = fields.IntField()
+    commentable = fields.BooleanField()
+    isrc = fields.StringField()
+    key_signature = fields.StringField()
+    comment_count = fields.LongField()
+    download_count = fields.LongField()
+    playback_count = fields.LongField()
+    favoritings_count = fields.LongField()
+    # not stored: original format (file format of track)
+    # not stored: original content size
+    # not stored: created_with (app the track was created with)
+    # not stored: asset_data (binary data of audio file)
+    # not stored: artwork_data (binary data of artwork image)
+    # not stored: user_favorite (track favorite of current user (boolean, authenticated requests only))
 
+    # Subresources
+    # comments
+    # favoriters
+    # secret-token ?
 
 # class Playlist(models.Model):
 #     # many to many with tracks
